31641fefa377cea0928e9b96511ad3b65364e9bd
EC2 Default User <ec2-user@ip-172-31-21-137.ap-northeast-1.compute.internal>
2023-03-18 13:02:00 +0000

    [Fix]エラー改善

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+
+  def index
+    @user = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @books = @user.books
@@ -17,6 +22,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :progile_image)
+    params.require(:user).permit(:name, :profile_image)
   end
 end
